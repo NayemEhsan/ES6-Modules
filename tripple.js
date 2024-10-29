7ad6f1cab20dea86b88800761860774017411f14
NayemEhsan <naymakon@gmail.com>
2024-10-29 21:05:34 +0600

    scope-hoisting

---
+++ tripple.js
@@ -0,0 +1,11 @@
+const first = 2;
+const second = "2";
+
+//  === check value and type both but == equal check only value
+if(first == second){
+    console.log("values are equal")
+}else{
+    console.log("Values are not equal")
+}
+
+// Block scope, Hoisting only for var declare in the scope 
